82b1d0883149dd0f3c69d00c42be58e14a47c597
Mila <lakralima@mail.ru>
2022-09-02 23:04:34 +0300

    Start new task

---
+++ Hometask01/Hometask01.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int num = new Random (). Next (100, 1000);
+Console.WriteLine(num);
+int numB = num / 100;
+Console.Write(numB); // первое число
+int numA = num % 10;// последнее число
+Console.Write(numA);

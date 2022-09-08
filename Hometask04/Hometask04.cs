a7aab77cf3aa47bd7e8df7b7bf21bfbf9753bf48
Mila <lakralima@mail.ru>
2022-09-09 00:09:07 +0300

    A little bit closer to the answer

--- Hometask04/Hometask04.cs
+++ Hometask04/Hometask04.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Console.WriteLine("Введите пятизначное число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int inversNum = 0;
+// Console.Write(num % 10);
+// while ((num /= 10) != 0) 
+// {
+//     Console.Write(num % 10);
+// }
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int inversNum = num % 10;
+Console.Write(inversNum);
+while ((num /= 10) != 0) 
+{
+    inversNum = num % 10;
+    Console.Write(inversNum);
+}
+if (num)

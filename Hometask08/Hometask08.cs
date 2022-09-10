f3207ef1f5164788cfef545f8472b1fb39cc8a8e
Mila <lakralima@mail.ru>
2022-09-10 17:31:53 +0300

    Task 27 is ready

---
+++ Hometask08/Hometask08.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите любое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int sumnum = 0;
+while (num > 0) 
+{
+    sumnum = sumnum + num % 10;
+    num /= 10;
+} 
+
+Console.WriteLine($"{sumnum} - это сумма цифр во введённом числе");

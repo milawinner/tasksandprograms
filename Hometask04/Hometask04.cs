59e45f099ea76581400b649b9b7dc408fb7367aa
Mila <lakralima@mail.ru>
2022-09-10 14:15:54 +0300

    Task19 with question

--- Hometask04/Hometask04.cs
+++ Hometask04/Hometask04.cs
@@ -4,15 +4,6 @@
 // 12821 -> да
 // 23432 -> да
 
-// Console.WriteLine("Введите пятизначное число");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int inversNum = 0;
-// Console.Write(num % 10);
-// while ((num /= 10) != 0) 
-// {
-//     Console.Write(num % 10);
-// }
-
 Console.WriteLine("Введите пятизначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 int inversNum = num % 10;
@@ -20,6 +11,9 @@ Console.Write(inversNum);
 while ((num /= 10) != 0) 
 {
     inversNum = num % 10;
-    Console.Write(inversNum);
-}
-if (num)
+} 
+// if (inversNum == num) 
+// {
+//     Console.WriteLine($"{num} является палиндромом");
+// }
+// else Console.WriteLine($"{num} не является палиндромом");

91a2a77c2f43fba6113c643be3a83fe2d09c60ea
Mila <lakralima@mail.ru>
2022-09-10 15:51:04 +0300

    Task 25 is ready

--- Hometask07/Hometask07.cs
+++ Hometask07/Hometask07.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
+// в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int ab = 1;
+for (int i = 1; i <= b; i++)
+{
+    ab *= a;
+}
+Console.WriteLine($"{ab} - это {a} в степени {b}");

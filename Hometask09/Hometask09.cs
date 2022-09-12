b7e223a807e39413ac64182f9cf60c359f792f20
Mila <lakralima@mail.ru>
2022-09-12 14:08:23 +0300

    Task 29 is ready

--- Hometask09/Hometask09.cs
+++ Hometask09/Hometask09.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] massiv = new int[8];
+for (int i = 0; i < massiv.Length; i++)
+{
+    massiv[i] = new Random().Next(0, 100);
+    Console.Write($" {massiv[i]} ");
+}

780a9eaa96ab75d9b8b220755e4ba0d1dc136fe5
Mila <lakralima@mail.ru>
2022-09-22 23:34:55 +0300

    Task 41 is ready

--- Hometask13/Hometask13.cs
+++ Hometask13/Hometask13.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+int quantity = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < quantity; i++)
+{
+    Console.WriteLine("Bведите число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) count++;
+}
+Console.WriteLine($"Количество введённых чисел больше 0: {count}");

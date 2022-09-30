9e90c1d4584ed64110f4fd4062a037e76c18682c
Mila <lakralima@mail.ru>
2022-09-30 17:11:32 +0300

    Task 66 is ready

--- Hometask19/Task66/Program.cs
+++ Hometask19/Task66/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое целое положительное число");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое положительное число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumOfAllNumbersBetween(int M, int N)
+{
+    if (M < N)
+    {
+        return SumOfAllNumbersBetween(M + 1, N) + M;
+    }
+    else if (M > N)
+    {
+        return SumOfAllNumbersBetween(N + 1, M) + N;
+    }
+    else 
+    {
+        return M;
+    }
+}
+Console.Write("Сумма натуральных чисел между введёнными числами: ");
+Console.WriteLine(SumOfAllNumbersBetween(M, N));

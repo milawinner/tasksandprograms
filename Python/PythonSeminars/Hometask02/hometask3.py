3330821cc68c5825358369387c7100864c9b1968
Mila <lakralima@mail.ru>
2022-11-29 23:45:41 +0300

    Task3 travolting

--- Python/PythonSeminars/Hometask02/hometask3.py
+++ Python/PythonSeminars/Hometask02/hometask3.py
@@ -1,8 +1,20 @@
 # Реализуйте алгоритм перемешивания списка. 
 # Встроенный алгоритм SHUFFLE не использовать! Реализовать свой метод
 
-n = int(input('Введите количество элементов списка (лучше не больше 10 для наглядности): '))
+import random
+
+n = int(input('Введите количество элементов списка (лучше небольшое для наглядности): '))
 my_list = []
-for i in range(-n, n+1):
+for i in range(1, n+1):
     my_list.append(i)
 print(my_list)
+
+my_list2 = my_list
+list_len = len(my_list2)
+for i in range(list_len):
+    temp = my_list2[random(list_len - 1)]
+    my_list2[i] = temp
+    temp = my_list2[i]
+print(my_list2)
+
+

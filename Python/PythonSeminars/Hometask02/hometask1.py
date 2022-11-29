c4612575524dd0b4c12dc6ddedece5f122987217
Mila <lakralima@mail.ru>
2022-11-29 20:27:09 +0300

    Hometask1 is ready, but not full

--- Python/PythonSeminars/Hometask02/hometask1.py
+++ Python/PythonSeminars/Hometask02/hometask1.py
@@ -2,3 +2,14 @@
 # Пример:
 # 6782 -> 23
 # 0,56 -> 11
+
+num = int(input('Введите число: '))
+sumnum = 0
+while (num > 0): 
+    sumnum = sumnum + num % 10
+    num //= 10
+print(f'{sumnum} - это сумма цифр во введённом числе.')
+
+# for i in num:
+#     sumnum += int(i)
+# print(sumnum)

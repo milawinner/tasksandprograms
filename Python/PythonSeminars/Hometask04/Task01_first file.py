6c5f74321e73b9de94d151de4ce6e5e139660e51
Mila <lakralima@mail.ru>
2022-12-08 09:34:23 +0300

    Start task. Not ready

--- Python/PythonSeminars/Hometask04/Task01_first file.py
+++ Python/PythonSeminars/Hometask04/Task01_first file.py
@@ -3,13 +3,29 @@
 # Пример: 
 # если k = 2, то многочлены могут быть => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-
 import random
 
 num = int(input('Введите степень для многочлена: '))
-
 x = ''
 for k in range(num, 0, -1):
     x += f'{random.randint(0,100)}*x**{k} + '
-
+    # if k == 1:
+    #     x += f'{random.randint(0,100)}*x'
+    # Здесть попыталась убрать плюс на конце многочлена, но не получается. 
+    # По такой формуле + уходит, но появляется два ненужных слагаемый.
 print(x, '= 0')
+
+#Здесь очищаю список от плюсов, остаются 4 элемента для суммы многочлена.
+for_sum1 = x.split()
+print(for_sum1)
+del(for_sum1[7])
+del(for_sum1[5])
+del(for_sum1[3])
+del(for_sum1[1])
+print(for_sum1)
+
+
+# for_sum1_dictionary = dict.fromkeys(for_sum1, "*x**")
+# print(for_sum1_dictionary)
+
+

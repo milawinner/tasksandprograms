0e3401e216648c53f25f3077a5b600a59f3b52d4
Mila <lakralima@mail.ru>
2022-12-12 12:23:15 +0300

    Task with candies ready, but not for clever robot

--- Python/PythonSeminars/Hometask05/hometask01.py
+++ Python/PythonSeminars/Hometask05/hometask01.py
@@ -6,30 +6,36 @@
 # b) Подумайте как наделить бота ""интеллектом""
 
 import random
+from random import randint
 
+print('Игра в конфеты. На столе лежит 150 конфет. Выигрывает тот, кто берёт последним.')
+
+player = 'Первым ходите Вы.'
+bot = 'Первый ходит робот.'
+first_or_second = randint(0,2)
+if first_or_second:
+    print(player)
+else:
+    print(bot)
 
 total = 150
 while total > 28:
-    player = int(input('Возьмите любое количество конфет, но не больше 28. Сколько берёте? '))
-    if player > 28:
-        print('Ай-ай-ай. Не больше 28 конфет, сладкоежка! Давай ещё раз. Сколько возьмёшь? ')
-        player = int(input())
-        total -= player
-    else: total -= player
-    print(f'Осталось {total} конфет из 150.')
-    bot = random.randint(1, 28)
-    print(f'Робот берёт {bot}.')
-    total -= bot
-    print(f'Осталось {total} конфет из 150.')
-while total < 28 and total > 0:
-         bot = total
-         print(f'Робот берёт {bot}.')
-         print(f'Робот победил!')
-         player = int(input('Возьмите любое количество конфет, но не больше оставшегося количества. Сколько берёте? '))
-         if player > total:
-            print('Ай-ай-ай. Здесь нет столько конфет! Давай ещё раз. Сколько возьмёшь? ')
+    if first_or_second:
+        player = int(input('Возьмите любое количество конфет, но не больше 28. Сколько берёте? '))
+        if player > 28:
+            print('Ай-ай-ай. Не больше 28 конфет, сладкоежка! Давай ещё раз. Сколько возьмёшь? ')
             player = int(input())
-            total -= player
-            print(f'Осталось {total} конфет из 150.')
-if player > total or bot > total:
-    print('Стоп-игра! Это был последний ход. The winner takes it all!')
+        total -= player
+        first_or_second = False
+        print(f'Осталось {total} конфет из 150.')
+    else: 
+        bot = random.randint(1, 28)
+        print(f'Робот берёт {bot}.')
+        total -= bot
+        first_or_second = True
+        print(f'Осталось {total} конфет из 150.')
+
+if first_or_second:
+    print(f"Выиграли вы!")
+else:
+    print(f"Выиграл робот! Восстание машин близко!")
